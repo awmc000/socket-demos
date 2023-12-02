0026d388ca6a9752f37abbbb8a13f122b99645e5
awmc000 <mccolmalex@gmail.com>
2023-12-02 15:31:41 -0800

    started sketching chatServer.py

---
+++ datagram/python/chatServer.py
@@ -0,0 +1,31 @@
+'''
+chatServer
+
+Hosts a 2-person text chat that also allows sending files.
+
+Clients send message to server, server sends message to
+other clients?
+
+         ____  Client1
+        /
+Server
+        \____  Client2
+
+
+'''
+import sys, socket
+# TODO: Implement server logic as follows.
+# 1. Create a new socket
+# 2. Attach a local address to a socket (bind)
+# 3. Announce willingness to accept connections (listen)
+# 4. Block caller until a connection request arrives (accept)
+# 5. Actively attempt to establish a connection (connect)
+# 6. Send some data over connection (send)
+# 7. Receive some data over connection (receive)
+# 8. Release the connection (close)
+
+def main():
+    return 0
+
+if __name__ == "main":
+    sys.exit(main)
